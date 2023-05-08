36ef7829be2c2af638d182a598e09b2e65a57759
chrismo <chrismo@clabs.org>
2023-05-08 11:04:44 -0500

    Unique, labeled cards in tuples.
    This is groovy. Now just need to add some logic to have it actually test
    the player.

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-console.log('Hello!');
-
 const button = document.querySelector('input');
 button.addEventListener('click', loadTuple);
+let cards = {};
+generateCards();
+loadTuple();
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -9,19 +10,52 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function randomTupleIndexes() {
+  let result = [];
+  let rand;
+  while (result.length < 3) {
+    rand = getRandomInt(1, 81);
+    if (result.indexOf(rand) === -1) result.push(rand);
+  }
+  return result;
+}
+
+function generateCards() {
+  let fills = ["solid", "striped", "clear"];
+  let shapes = ["squiggle", "diamond", "pill"];
+  let colors = ["red", "purple", "green"];
+  let numbers = [1, 2, 3];
+
+  let cardIndex = 1;
+  fills.forEach(fill => shapes.forEach(shape => colors.forEach(color => numbers.forEach(number => {
+    cards[cardIndex] = {
+      fill: fill,
+      shape: shape,
+      color: color,
+      number: number
+    };
+    cardIndex++;
+  }))));
+}
+
+
 function loadTuple() {
-  let div = document.querySelector('#tuple');
+  let div = document.querySelector('#tuple'), indexes;
   while (div.firstChild) {
     div.removeChild(div.firstChild);
   }
 
-  for (var i = 0; i < 3; i++) {
-    let a = getRandomInt(1, 81);
-  
-    let imgA = document.createElement("img");
-    imgA.src = `https://www.setgame.com/sites/all/modules/setgame_set/assets/images/new/${a}.png`;
-  
-    div.appendChild(imgA);
-  }
+  indexes = randomTupleIndexes();
 
+  indexes.forEach(cardIndex => {
+    let card = cards[cardIndex];
+
+    let img = document.createElement("img"), plural;
+    img.src = `https://www.setgame.com/sites/all/modules/setgame_set/assets/images/new/${cardIndex}.png`;
+    plural = card.number === 1 ? '' : 's';
+    let cardDescription = `${card.number} ${card.color} ${card.fill} ${card.shape}${plural}`;
+    img.alt = cardDescription;
+    img.title = cardDescription;
+    div.appendChild(img);
+  });
 }
