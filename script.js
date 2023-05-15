497cecb945a6362335eefa70e430857a26d74da3
chrismo <chrismo@clabs.org>
2023-05-15 13:34:56 -0500

    Separate of game pages
    Fast 30 still works, zen doesn't work at all, and will need a lot of
    refactoring of functionality out of fast_30.js back into shared content.

--- script.js
+++
@@ -1,6 +0,0 @@
-import {GameBase} from './js/game_base.js'
-import {Practice} from './js/practice.js'
-import {Fast30} from './js/fast_30.js'
-
-let gameBase = new GameBase(document);
-let currentGame = new Fast30(gameBase);
